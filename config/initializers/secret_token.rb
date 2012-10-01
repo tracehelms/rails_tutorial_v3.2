236269dc19e33bac210be2b799b76d7d610e67c8
tracehelms-zit <trace.helms@zalkit.com>
2012-10-01 09:40:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '43b3acdc4fdc783593c4948fe0e2243f499ab5bbd105f964367be83da0f24d3f6205e8ac14422ebf074f4036a514b2f3ffc953313cbaf5f10643ca018e242bd0'
