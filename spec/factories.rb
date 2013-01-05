39a7aa39381e1f23d7e33d81039bb3cb992a637a
tracehelms <tracehelms@gmail.com>
2013-01-05 12:36:07 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Trace Helms"
+    email                 "ex@ex.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
